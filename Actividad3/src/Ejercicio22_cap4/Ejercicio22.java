25be18eda68e5ddcfe92e4cb4fce12b558c4b810
restr <restr@Manuel-R>
2025-01-14 00:01:45 -0500

    new change

---
+++ Actividad3/src/Ejercicio22_cap4/Ejercicio22.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio22_cap4;
+import java.util.Scanner;
+
+
+public class Ejercicio22 {
+    public static void main(String[] args) {
+        String NOM;
+        double salhora,numhoras, salariototal;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese el nombre del empleado: ");
+        NOM = entrada.next();
+        System.out.println("Ingrese el nombre del empleado: ");
+        salhora = entrada.nextDouble();
+        System.out.println("Ingrese el nombre del empleado: ");
+        numhoras = entrada.nextDouble();
+        
+        
+        
+        
+        
+    }
+}
